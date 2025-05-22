48580e472f3a42b39ae00c5c4467d67a1bd7891a
M0R4D-Tiferras-self-learning-account <m.tiferras@gmail.com>
2025-05-22 15:35:14 +0100

    Task 2

--- 2-arguments.js
+++ 2-arguments.js
@@ -4,6 +4,8 @@ let argc = process.argv.length;
 
 if (argc === 2) {
 	console.log("No argument")
+} else if (argc === 3) {
+	console.log("Argument found")
 } else {
 	console.log("Arguments found")
 }
